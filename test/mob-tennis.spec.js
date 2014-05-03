5db5cf32d844117c357e8bf3b93d2a9ac17467ee
Jackson Zhang <zbcjackson@gmail.com>
2014-05-03 14:38:23 +0800

    Add test framework.

---
+++ test/mob-tennis.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('mob-tennis', function() {
+    describe('joseph', function() {
+        it('should display love all when 0 : 0', function () {
+            assert.equal(true, false);
+        });
+    });
+})
