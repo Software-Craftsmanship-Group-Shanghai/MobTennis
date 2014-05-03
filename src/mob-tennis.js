70c12463c4c845340c274b820f25e3885a56c4e7
Jackson Zhang <zbcjackson@gmail.com>
2014-05-03 14:55:55 +0800

    Rename functionName

--- src/mob-tennis.js
+++ src/mob-tennis.js
@@ -1,14 +1,14 @@
 exports.Match = function() {
   this.player1score = 0;
 
-  this.CurrentScore = function() {
+  this.currentScore = function() {
     if(this.player1score != 0) {
         return 'Fifteen Love';
     }
     return 'Love All';
   }
 
-  this.Player1Score = function() {
+  this.player1Score = function() {
     this.player1score++;
   }
 }
--- test/mob-tennis.spec.js
+++ test/mob-tennis.spec.js
@@ -3,12 +3,12 @@ var Match = require('../src/mob-tennis').Match;
 describe('mob-tennis', function() {
     it('should display Love All when 0 : 0', function () {
         var m = new Match();
-        assert.equal('Love All', m.CurrentScore());
+        assert.equal('Love All', m.currentScore());
     });
 
     it('should display Fifteen Love when 15 : 0', function () {
         var m = new Match();
-        m.Player1Score();
-        assert.equal('Fifteen Love', m.CurrentScore());
+        m.player1Score();
+        assert.equal('Fifteen Love', m.currentScore());
     })
 })
