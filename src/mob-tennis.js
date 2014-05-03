e4741167713d88fc19c459edc470363403b0c09b
Jackson Zhang <zbcjackson@gmail.com>
2014-05-03 14:50:54 +0800

    Add Fifteen Love.

--- src/mob-tennis.js
+++ src/mob-tennis.js
@@ -1,6 +1,15 @@
 exports.Match = function() {
+  this.player1score = 0;
+
   this.CurrentScore = function() {
+    if(this.player1score != 0) {
+        return 'Fifteen Love';
+    }
     return 'Love All';
+  }
+
+  this.Player1Score = function() {
+    this.player1score++;
   }
 }
 
--- test/mob-tennis.spec.js
+++ test/mob-tennis.spec.js
@@ -1,10 +1,14 @@
 var assert = require('assert');
 var Match = require('../src/mob-tennis').Match;
 describe('mob-tennis', function() {
-    describe('joseph', function() {
-        it('should display love all when 0 : 0', function () {
-            var m = new Match();
-            assert.equal('Love All', m.CurrentScore());
-        });
+    it('should display Love All when 0 : 0', function () {
+        var m = new Match();
+        assert.equal('Love All', m.CurrentScore());
     });
+
+    it('should display Fifteen Love when 15 : 0', function () {
+        var m = new Match();
+        m.Player1Score();
+        assert.equal('Fifteen Love', m.CurrentScore());
+    })
 })
